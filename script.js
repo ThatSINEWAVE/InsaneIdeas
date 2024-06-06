383d6abaf0cbfba635b79633d085fb0aa89c7177
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-06-07 02:39:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const anotherBtn = document.getElementById('anotherBtn');
 const descriptionEl = document.getElementById('description');
 const ideaTextEl = document.getElementById('ideaText');
 
-const apiUrl = 'https://itsthisforthat.com/api.php?json';
+const apiUrl = 'https://corsproxy.io/?https://itsthisforthat.com/api.php?json';
 
 inspireBtn.addEventListener('click', getIdea);
 anotherBtn.addEventListener('click', getIdea);
@@ -13,17 +13,10 @@ function getIdea() {
   inspireBtn.style.display = 'none';
   anotherBtn.style.display = 'block';
 
-  fetch('https://itsthisforthat.com/api.php?json', { mode: 'no-cors' })
-    .then(response => {
-      if (response.ok) {
-        return response.text();
-      } else {
-        throw new Error('Request failed with status: ' + response.status);
-      }
-    })
+  fetch(apiUrl)
+    .then(response => response.json())
     .then(data => {
-      const parsedData = JSON.parse(data);
-      const { this: thisValue, that: thatValue } = parsedData;
+      const { this: thisValue, that: thatValue } = data;
       ideaTextEl.textContent = `You should make a ${thisValue} that will be used for ${thatValue}.`;
     })
     .catch(error => {
